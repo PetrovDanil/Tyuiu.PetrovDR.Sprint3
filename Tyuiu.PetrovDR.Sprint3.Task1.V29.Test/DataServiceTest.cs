1957b846f1ac50a58316b53cb4046bb5f1571f2d
PetrovDanil <Tyuiu.PetrovDR@outlook.com>
2025-10-30 22:23:10 +0500

    task 1

---
+++ Tyuiu.PetrovDR.Sprint3.Task1.V29.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.PetrovDR.Sprint3.Task1.V29.Lib;
+
+namespace Tyuiu.PetrovDR.Sprint3.Task1.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 11;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+
+            double wait = 13051.34;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PetrovDR.Sprint3.Task1.V29/Program.cs
@@ -0,0 +1,53 @@
+﻿using Tyuiu.PetrovDR.Sprint3.Task1.V29.Lib;
+
+
+namespace Tyuiu.PetrovDR.Sprint3.Task1.V29
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            void PrintCenteredLine(string text, int totalWidth)
+            {
+                var padding = totalWidth - text.Length - 2;
+                Console.WriteLine($"* {text}{new string(' ', padding)}*");
+            }
+            var width = 100;
+
+
+
+            DataService ds = new DataService();
+
+            PrintCenteredLine("Спринт #3 | Выполнил: Петров Д. Р. | РППб-25-1", width);
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("Спринт #3", width);
+            PrintCenteredLine("Тема: Алгоритмы циклической структуры", width);
+            PrintCenteredLine("Задание #1", width);
+            PrintCenteredLine("Вариант #29", width);
+            PrintCenteredLine("Выполнил: Петров Данил Романович | РППб-25-1", width);
+            Console.WriteLine(new string('*', width));
+
+            PrintCenteredLine("УСЛОВИЕ:", width);
+            Console.WriteLine("Написать программу используя цикл while, которая вычисляет произведение ряда по формуле, при х = 0,25.", width);
+            Console.WriteLine(new string('*', width));
+
+            PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ:", width);
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 11;
+
+            Console.WriteLine("x = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("РЕЗУЛЬТАТ:", width);
+
+            Console.WriteLine("Сумма ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
