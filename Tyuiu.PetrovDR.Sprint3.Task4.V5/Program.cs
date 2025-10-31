71b30a47955239e9dbf83a475ac1b3ee936a13c0
PetrovDanil <Tyuiu.PetrovDR@outlook.com>
2025-11-01 00:00:12 +0500

    task 4

---
+++ Tyuiu.PetrovDR.Sprint3.Task4.V5/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.PetrovDR.Sprint3.Task4.V5.Lib;
+
+
+namespace Tyuiu.PetrovDR.Sprint3.Task4.V5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            void PrintCenteredLine(string text, int totalWidth)
+            {
+                var padding = totalWidth - text.Length - 2;
+                Console.WriteLine($"* {text}{new string(' ', padding)}*");
+            }
+            var width = 100;
+
+
+
+            DataService ds = new DataService();
+
+            PrintCenteredLine("Спринт #3 | Выполнил: Петров Д. Р. | РППб-25-1", width);
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("Спринт #3", width);
+            PrintCenteredLine("Тема: Алгоритмы циклической структуры", width);
+            PrintCenteredLine("Задание #4", width);
+            PrintCenteredLine("Вариант #5", width);
+            PrintCenteredLine("Выполнил: Петров Данил Романович | РППб-25-1", width);
+            Console.WriteLine(new string('*', width));
+
+            PrintCenteredLine("УСЛОВИЕ:", width);
+            Console.WriteLine("На отрезке, где x принимает значения от -5 до 5, вычислить значение функции", width);
+            Console.WriteLine("y=(cos(x)-sin(x))/x. При х = 0 прервать цикл. Полученные значения суммировать.", width);
+            Console.WriteLine(new string('*', width));
+
+            PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ:", width);
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("РЕЗУЛЬТАТ:", width);
+
+            Console.WriteLine("Сумма значений = " + ds.Calculate(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
